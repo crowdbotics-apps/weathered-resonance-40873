bf87d751c8e5e2a12408eaa4042e5859a46770d2
Crowdbotics <team@crowdbotics.com>
2023-05-21 15:38:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_resonance_40873/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_resonance_40873;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_resonance_40873";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-resonance-40873",
+  url: "https://weathered-resonance-40873.botics.co",
+  api: "https://weathered-resonance-40873.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
